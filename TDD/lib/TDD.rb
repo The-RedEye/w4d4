82ad0bb2a5e69e503870b1f74ccf2d6f9527ca90
Tristan Angeles <setswipe@gmail.com>
2021-09-02 14:01:27 -0400

    Initial spec for my_uniq method created; working on actual my_uniq method

---
+++ TDD/lib/TDD.rb
@@ -0,0 +1,9 @@
+
+def my_uniq(arr)
+
+#sorting through
+#removing duplicates found
+
+#return array without duplicates
+
+end
--- TDD/spec/TDD_spec.rb
+++ TDD/spec/TDD_spec.rb
@@ -2,3 +2,19 @@ require "TDD"
 require "rspec"
 
 
+
+  describe "#my_uniq" do
+
+    it "takes in an array" do
+      expect(my_uniq([1,2,3])).to raise_error(ArgumentError)
+    end
+
+    it "returns an array" do
+      expect(my_uniq([1,2,3])).to be_a(Array)
+    end
+
+    it "returns an array with no duplicates" do
+      expect( my_uniq([1,2,1,3,4,3]) ).to eq( [1,2,3,4] )
+    end
+
+  end
